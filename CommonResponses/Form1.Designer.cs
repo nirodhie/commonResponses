59ec7822dc98cf184758aa3c436eb9f1b5d9af79
Lukasz Teska <hclteska@BD.com>
2019-11-07 09:33:57 +0100

    Add project files.

---
+++ CommonResponses/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace CommonResponses
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(214, 756);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Copy to clipboard";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(314, 10);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(939, 740);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Choose one of the responses visible to the left";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1033, 756);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(220, 37);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Save this response";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Location = new System.Drawing.Point(5, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 36);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "MyLocation";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(675, 759);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(352, 26);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Visible = false;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(447, 756);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(222, 35);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "Send via Outlook";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Visible = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(13, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(295, 698);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Responses";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1258, 797);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Common responses";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ CommonResponses/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Outlook;
+using OutlookApp = Microsoft.Office.Interop.Outlook.Application;
+
+
+
+namespace CommonResponses
+{
+    public partial class Form1 : Form
+    {
+
+        string ResponseName = "";
+        string pathToResponses = @"c:\users\hclteska\responses\";
+        public int responsesCount = Directory.GetFiles(@"c:\users\hclteska\responses\", "*.txt", SearchOption.TopDirectoryOnly).Length;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "You have " +((responsesCount+1).ToString())+" files with responses" ;
+
+           
+            string[] fileNames = new DirectoryInfo(pathToResponses).GetFiles().Select(o => o.Name).ToArray();
+
+
+            Point newLoc = new Point(5, 15);
+            for (int i = 0; i< (responsesCount); i++)
+            {
+                RadioButton rdo = new RadioButton();
+                rdo.Width = 150; //szerokość tekstu przy radiobuttonie
+                rdo.Name = "RadioButton" + i;
+                rdo.Text = Path.GetFileNameWithoutExtension(fileNames[i]);
+                rdo.Location = newLoc;
+                newLoc.Offset(0, 20);
+                groupBox1.Controls.Add(rdo);
+
+                rdo.Click += delegate
+                {
+                    textBox1.Text = File.ReadAllText(pathToResponses + rdo.Text + ".txt");  
+                };
+            }
+
+            
+        }
+
+
+        protected void button_Click(object sender, EventArgs e)
+        {
+            RadioButton button = sender as RadioButton;
+            textBox1.Text = button.Text;
+            // identify which button was clicked and perform necessary actions
+        }
+
+
+        private void checkChanged(object sender, EventArgs e)
+        {
+            foreach (RadioButton rdo in groupBox1.Controls)
+            {
+                if (rdo.Checked)
+                    textBox1.Text = rdo.Text;
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            File.WriteAllText(pathToResponses + textBox2.Text + ".txt", textBox1.Text);
+        }
+
+       
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            Button btn = (Button)sender;
+            ResponseName = btn.Text;
+            textBox1.Text = File.ReadAllText(pathToResponses + ResponseName + ".txt");
+            button1.Visible = true;
+            textBox2.Visible = true;
+            textBox2.Text = ResponseName;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button2.Visible = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            ResponseName = btn.Text;
+            textBox1.Text = File.ReadAllText(pathToResponses + ResponseName + ".txt");
+            button1.Visible = true;
+            textBox2.Visible = true;
+            textBox2.Text = ResponseName;
+        }
+
+        
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            ResponseName = btn.Text;
+            textBox1.Text = File.ReadAllText(pathToResponses + ResponseName + ".txt");
+            button1.Visible = true;
+            textBox2.Visible = true;
+            textBox2.Text = ResponseName;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            ResponseName = btn.Text;
+            textBox1.Text = File.ReadAllText(pathToResponses + ResponseName + ".txt");
+            button1.Visible = true;
+            textBox2.Text = ResponseName;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            ResponseName = btn.Text;
+            textBox1.Text = File.ReadAllText(pathToResponses + ResponseName + ".txt");
+            button1.Visible = true;
+            textBox2.Visible = true;
+            textBox2.Text = ResponseName;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            ResponseName = btn.Text;
+            textBox1.Text = File.ReadAllText(pathToResponses + ResponseName + ".txt");
+            button1.Visible = true;
+            textBox2.Visible = true;
+            textBox2.Text = ResponseName;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+            OutlookApp outlookApp = new OutlookApp();
+            MailItem mailItem = outlookApp.CreateItem(OlItemType.olMailItem);
+
+            mailItem.Subject = ResponseName;
+            mailItem.Body = textBox1.Text;
+            mailItem.Display(true);
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            ResponseName = btn.Text;
+            textBox1.Text = File.ReadAllText(pathToResponses + ResponseName + ".txt");
+            button1.Visible = true;
+            textBox2.Visible = true;
+            textBox2.Text = ResponseName;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click_1(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            ResponseName = btn.Text;
+            textBox1.Text = File.ReadAllText(pathToResponses + ResponseName + ".txt");
+            button1.Visible = true;
+            textBox2.Visible = true;
+            textBox2.Text = ResponseName;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            ResponseName = btn.Text;
+            textBox1.Text = File.ReadAllText(pathToResponses + ResponseName + ".txt");
+            button1.Visible = true;
+            textBox2.Visible = true;
+            textBox2.Text = ResponseName;
+        }
+    }
+}
